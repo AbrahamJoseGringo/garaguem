3ec2d45ac20e4fa669a44691ad2798c012000c71
Nome do usuário <aperez.castro32@gmail.com>
2024-04-01 11:55:41 -0300

    hoje

--- app/urls.py
+++ app/urls.py
@@ -18,10 +18,10 @@ from core.views import MarcaViewSet
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet, basename="users")
-router.register(r"acessorio", AcessorioViewSet)
-router.register(r"categoria", CategoriaViewSet)
-router.register(r"cor", CorViewSet)
-router.register(r"marca", MarcaViewSet)
+router.register(r"acessorios", AcessorioViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"cores", CorViewSet)
+router.register(r"marcas", MarcaViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     # OpenAPI 3
